fae21c699745c958a55f5fe83dfbce8741542105
Felipe Cardoso <agn0stkp@gmail.com>
2023-10-09 03:10:43 +0000

    add solution for iterators

---
+++ iterators/iterators4.rs
@@ -0,0 +1,39 @@
+// iterators4.rs
+//
+// Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
+// hint.
+
+pub fn factorial(mut num: u64) -> u64 {
+    if num == 0 {
+        1
+    } else {
+        for i in 1..num {
+            num *= i;
+        }
+        num
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(1, factorial(0));
+    }
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(1, factorial(1));
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(2, factorial(2));
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(24, factorial(4));
+    }
+}
